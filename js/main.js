7d0026a7cee8307e5e28652a4a1d14d0eb73c95e
mahmoud medhat <mahmodmht.8.8@gmail.com>
2023-04-18 13:31:45 +0200

    the 3th week is done

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// let d = "-100";
+// let e = "20";
+// let f = 30;
+// let g = true;
+// console.log(-d * +e);
+// console.log(++e * ++g + ++f + -d);
+// Replace ? With Arithmetic Operators
+console.log(10 * 20 + (15 % 3) + 190 + 10 - 400);
+
+//
+
+// let num = 3;
+
+// // Solution One
+// console.log(num * num - num); // 6
+
+// // Solution Two
+// console.log(num + num); // 6
+
+// // Soultion Three
+
+// // Soultion Four
+// console.log(num * num + -num); // 6
+
+// // Solution Five
+// console.log((num - true) * num); // 6
+
+// // Solution Six
+// console.log(); // 6
+// console.log(num * --num); // 6
+
+//
+
+let points = 10;
+
+// Write Your Code Here
+points = ++points;
+points = ++points;
+points = ++points;
+console.log(points); // 13
+
+// Write Your Code Here
+// points = --points;
+// points = --points;
+// console.log(points); // 8;
+
+//
+
+let num = "10";
+
+// Solution One
+console.log(+num + +num); // 20
+
+// Solution Two
+console.log(); // 20
+
+// Solution Three
+console.log(); // 20
+
+// Solution Four
+console.log("Write Your Code Here"); // 20
